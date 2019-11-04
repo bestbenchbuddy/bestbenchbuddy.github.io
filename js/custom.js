5bea35bb282169e27ba1f71d352a177cf4a5cb88
HonkeyGuy <nickpatrickbastian@gmail.com>
2019-11-04 14:33:19 -0500

    Created multiple models so that each project would have its own description when selected. Added new images to each project

--- js/custom.js
+++ js/custom.js
@@ -104,8 +104,10 @@ $(document).ready(function () {
     });
 
     //animatedModal
-    $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
-
+    $("#demo01").animatedModal();
+	$("#demo02").animatedModal2();
+	$("#demo03").animatedModal3();
+	$("#demo04").animatedModal4();
     // Contact Form 	
 
     // validate contact form
